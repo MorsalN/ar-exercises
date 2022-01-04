2956c9826f06e3bff3d9a4ffa6528df3ec26a235
Morsal Niyaz <morsalniyaz@gmail.com>
2022-01-03 21:54:05 -0800

    Updated Stores

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,14 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+store1_update = Store.find_by(id:1)
+store1_update = @store1
+# store1_update.save
+
+store2_update = Store.find_by(id:2)
+store2_update = @store2
+# store2_update.save
+
+store1_update2 = Store.find_by(@store1)
+store1_update2.name = "Coquitlam"
+store1_update2.save
