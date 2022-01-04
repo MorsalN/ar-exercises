bfd0692cb56759294a0a6c867e11a5f497ade8c0
Morsal Niyaz <morsalniyaz@gmail.com>
2022-01-04 11:21:45 -0800

    Completed exec 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,20 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+# ONE WAY
+puts Store.sum(:annual_revenue)
+
+puts Store.average(:annual_revenue)
+
+puts Store.where("annual_revenue > 1000000").count
+
+#ANOTHER WAY
+@total_revenue = Store.where("annual_revenue > 0")
+sum = 0
+@total_revenue.each do |total|
+  sum += total.annual_revenue
+end 
+
+puts "The total revenue is $#{sum}."
+puts "The average annual revenue is $#{sum/6}"
